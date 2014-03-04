b02e2e10589447f5467ec05f76450977040c77d3
Patrick McGreevy & Brentwood Reid <student@epicodus.com>
2014-03-04 10:58:07 -0800

    achieve prime shifting functionality

--- legacy_converter/lib/legacy.rb
+++ legacy_converter/lib/legacy.rb
@@ -2,11 +2,9 @@ def convert_hash(hash)
   result = Hash.new
 
   hash.each do |key, val_ary|
-    row = Hash.new
     val_ary.each do |val|
-      row[val] = key
+      result[val] = key
     end
-    result.merge!(row)
   end
   result
 end
